904a1c3511b8b55bfb4d76ba0c716f161983958f
Alejandro Mendoza <zippi180@gmail.com>
2017-11-08 22:15:45 -0600

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -12,8 +12,6 @@ app.use(bodyParser.json());
 
 app.post('/todos', (req, res) =>{
 
-    console.log(req.body.text);
-
     const todo = new Todo({
         text: req.body.text
     });
@@ -24,6 +22,14 @@ app.post('/todos', (req, res) =>{
         res.status(400).send(e);
     });
 
+});
+
+app.get('/todos', (req, res) => {
+   Todo.find().then((todos) => {
+       res.send({todos});
+   }, (e) => {
+        res.status(400).send(e);
+   });
 });
 
 app.listen(3000, () => {
