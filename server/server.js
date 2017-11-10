35432397c71bb3733f833a81e68acd519072f717
Alejandro Mendoza <zippi180@gmail.com>
2017-11-09 22:55:31 -0600

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -52,8 +52,21 @@ app.get('/todos/:id', (req, res) => {
         return res.send({todo});
     }).catch((e) => res.status(400).send());
 
+});
 
+app.delete('/todos/:id', (req, res) => {
+    let id = req.params.id;
 
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if(!todo){
+            return res.status(404).send();
+        }
+        return res.send(todo);
+    }).catch((err) => res.status(400).send());
 });
 
 app.listen(port, () => {
