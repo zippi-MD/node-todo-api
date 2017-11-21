cf8e59542f74392bb46590a7aecab4349d782fb5
Alejandro Mendoza <zippi180@gmail.com>
2017-11-20 23:41:52 -0600

    Added test for POST /users/login

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -224,7 +224,8 @@ describe('POST /users', () => {
                 expect(user).toExist();
                 expect(user.password).toNotBe(password);
                 done();
-            })
+            }).catch((e) => done(e));
+
             });
     });
 
@@ -257,5 +258,59 @@ describe('POST /users', () => {
             })
             .end(done);
 
+    });
+});
+
+describe('POST /users/login', () => {
+    it('Should login user and return auth token', (done) => {
+        request(app)
+            .post('/users/login')
+            .send({
+                email: users[1].email,
+                password: users[1].password
+            })
+            .expect(200)
+            .expect((res) => {
+                expect(res.headers['x-auth']).toExist();
+            })
+            .end((err, res) => {
+                if(err){
+                   return done(err);
+                }
+
+                User.findById(users[1]._id).then((user) => {
+                    expect(user.tokens[0]).toInclude({
+                       access: 'auth',
+                       token: res.headers['x-auth']
+                    });
+                    done();
+                }).catch((e) => {
+                    done(e);
+                });
+            });
+    });
+
+    it('Should reject invalid login', (done) => {
+
+        request(app)
+            .post('/users/login')
+            .send({
+                email: users[1].email,
+                password: users[1].password + 'kiubas'
+            })
+            .expect(400)
+            .expect((res) => {
+                expect(res.headers['x-auth']).toNotExist();
+            })
+            .end((err, res) => {
+                if(err){
+                    return done(err);
+                }
+
+                User.findById(users[1]._id).then((user)=>{
+                    expect(user.tokens.length).toBe(0);
+                    done();
+                }).catch((err) => done(err));
+            });
     });
 });
